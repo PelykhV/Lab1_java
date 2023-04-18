5ca08783f57cd472cae9ff6eaa8308057ccf71a7
PelykhV <119511581+PelykhV@users.noreply.github.com>
2023-04-18 08:13:40 +0300

    changing main for our needs

--- src/java/ua/lviv/iot/algo/part1/lab3/HotelManager.java
+++ src/java/ua/lviv/iot/algo/part1/lab3/HotelManager.java
@@ -1,69 +1,58 @@
-    package ua.lviv.iot.algo.part1;
+        package ua.lviv.iot.algo.part1;
 
-    import lombok.var;
+        import java.io.IOException;
+        import java.io.PrintWriter;
+        import java.util.LinkedList;
+        import java.util.ArrayList;
+        import java.util.List;
+        import java.util.stream.Collectors;
 
-    import java.util.ArrayList;
-    import java.util.Collection;
-    import java.util.List;
-    import java.util.stream.Collectors;
-    public class HotelManager {
-        private final List<Hotel> hotels = new ArrayList<>();
+        import static ua.lviv.iot.algo.part1.HotelWriter.file;
 
-        public void addHotel(Hotel hotel) {
-            hotels.add(hotel);
-        }
+        public class HotelManager {
+            private final List<Hotel> hotels = new ArrayList<>();
 
-        public List<Hotel> findHotelsWithRatingMoreThan(double rating) {
-            return hotels.stream().
-                    filter(hotel -> hotel.getRating() > rating).
-                    collect(Collectors.toList());
-        }
-
-        public List<Hotel> findHotelsWithTotalRoomsMoreThan(int totalRooms) {
-            return hotels.stream().
-                    filter(hotel -> hotel.getTotalRooms() > totalRooms).
-                    collect(Collectors.toList());
-        }
-
-        public static void main(String[] args) {
-            HotelManager hotelManager = new HotelManager();
-            hotelManager.addHotel(new ResortHotel("Monte Carlo", 465, 35, 4.6,
-                    "Paperoni", 3, 4, 6));
-            hotelManager.addHotel(new ResortHotel("New Orleano", 641, 146, 3.8,
-                    "Matrix", 6, 10, 10));
-            hotelManager.addHotel(new Motel("Pump", 444, 111, 5.0, 411, 114,
-                    "Katowice", "Lodz"));
-            hotelManager.addHotel(new Motel("Cloud", 1760, 600, 4.7, 927, 270,
-                    "Paris", "Lion"));
-            hotelManager.addHotel(new BusinessHotel("Light", 311, 19, 4.7,
-                    30, "Samsung"));
-            hotelManager.addHotel(new BusinessHotel("Seven", 100, 50, 4.2,
-                    20, "LG"));
-            hotelManager.addHotel(new SpaHotel("Dream", 60, 56, 4.1,
-                    3, "Fly"));
-            hotelManager.addHotel(new SpaHotel("Rainbow", 70, 56, 4.8,
-                    5, "Winter"));
-
-            for (Hotel hotel : hotelManager.hotels) {
-                System.out.println(hotel);
+            public void addHotel(Hotel hotel) {
+                hotels.add(hotel);
             }
 
-            System.out.println("\n");
-
-            var hotelRating = hotelManager.findHotelsWithRatingMoreThan(4.5);
-            for (Hotel hotel: hotelRating){
-                System.out.println(hotel);
+            public List<Hotel> findHotelsWithRatingMoreThan(double rating) {
+                return hotels.stream().
+                        filter(hotel -> hotel.getRating() > rating).
+                        collect(Collectors.toList());
             }
 
-            System.out.println("\n");
+            public List<Hotel> findHotelsWithTotalRoomsMoreThan(int totalRooms) {
+                return hotels.stream().
+                        filter(hotel -> hotel.getTotalRooms() > totalRooms).
+                        collect(Collectors.toList());
+            }
+            public List<Hotel> getHotels() {
+                return hotels;
+            }
 
-            var hotelRooms = hotelManager.findHotelsWithTotalRoomsMoreThan(400);
-            for (Hotel hotel: hotelRooms){
-                System.out.println(hotel);
+            public static void main(String[] args) throws  IOException {
+                HotelWriter hotelWriter = new HotelWriter();
+                hotelWriter.printWriter = new PrintWriter(file);
+
+                List<Hotel> hotels = new ArrayList<>();
+                hotels.add(new ResortHotel("New Orleano", 641, 146, 3.8,
+                        "Matrix", 6, 10, 10));
+                hotels.add(new Motel("Pump", 444, 111, 5.0, 411, 114,
+                        "Katowice", "Lodz"));
+                hotels.add(new SpaHotel("Dream", 60, 56, 4.1,
+                        3, "Fly"));
+                hotels.add(new ResortHotel("Monte Carlo", 465, 35, 4.6,
+                        "Paperoni", 3, 4, 6));
+                hotels.add(new Motel("Cloud", 1760, 600, 4.7, 927, 270,
+                        "Paris", "Lion"));
+                hotels.add(new BusinessHotel("Seven", 100, 50, 4.2,
+                        20, "LG"));
+                hotels.add(new SpaHotel("Rainbow", 70, 56, 4.8,
+                        5, "Winter"));
+                hotels.add(new BusinessHotel("Light", 311, 19, 4.7,
+                        30, "Samsung"));
+
+                hotelWriter.writeToFile(hotels);
             }
         }
-        public List<Hotel> getHotels() {
-            return hotels;
-        }
-
-    }
