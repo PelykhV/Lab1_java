2ca58ee483f7356410caa6882149a03b0530df5b
PelykhV <119511581+PelykhV@users.noreply.github.com>
2023-04-18 08:12:38 +0300

    adding methods toCsv and getHeaders to class

--- src/java/ua/lviv/iot/algo/part1/lab3/SpaHotel.java
+++ src/java/ua/lviv/iot/algo/part1/lab3/SpaHotel.java
@@ -4,6 +4,7 @@ import lombok.ToString;
 
 @ToString
 public class SpaHotel extends Hotel{
+    public static final String HEADERS = "fitnessRooms, complexName";
     public int fitnessRooms;
     public String complexName;
     public SpaHotel(String name, int totalRooms, int availableRooms, double rating, int fitnessRooms,
@@ -11,10 +12,18 @@ public class SpaHotel extends Hotel{
         super(name,totalRooms,availableRooms,rating);
         this.fitnessRooms=fitnessRooms;
         this.complexName=complexName;
+        type = "SpaHotel";
     }
 
     @Override
     public String getLocation() {
         return complexName;
+    }
+    public String getHeaders() {
+        return super.getHeaders() + "," + HEADERS;
+    }
+
+    public String toCsv() {
+        return super.toCsv() + "," + fitnessRooms + "," + complexName;
     }
 }
