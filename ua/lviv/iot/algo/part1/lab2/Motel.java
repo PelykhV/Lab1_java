33919594d6886a66ea0a44a282dae7057b082975
PelykhV <119511581+PelykhV@users.noreply.github.com>
2023-06-22 11:50:36 +0300

    Update Motel.java

--- ua/lviv/iot/algo/part1/lab2/Motel.java
+++ ua/lviv/iot/algo/part1/lab2/Motel.java
@@ -2,7 +2,7 @@ package ua.lviv.iot.algo.part1.lab1;
 
 import lombok.ToString;
 
-@ToString
+@ToString(callSuper = true)
 public class Motel extends Hotel{
     private int trackNumber;
     private int trackKilometer;
@@ -10,13 +10,14 @@ public class Motel extends Hotel{
     private String cityTo;
 
     public Motel(String name, int totalRooms, int availableRooms, double rating, int trackNumber, int trackKilometer,
-                  String cityFrom, String cityTo){
+                 String cityFrom, String cityTo){
         super(name,totalRooms,availableRooms,rating);
         this.trackNumber=trackNumber;
         this.trackKilometer=trackKilometer;
         this.cityFrom=cityFrom;
         this.cityTo=cityTo;
     }
+
     @Override
     public String getLocation() {
         return cityFrom + "-" +cityTo + ", " + trackKilometer + "км";
